f9eac50371b884eb374cea09fe8ef57867cc5d56
Matthieu BDG <matthieu.boubeedegramont@limayrac.fr>
2022-12-19 11:00:22 +0100

    update remember me

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
@@ -27,6 +28,7 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+
     /**
      * @Route("/deconnexion", name="app_logout")
      */
